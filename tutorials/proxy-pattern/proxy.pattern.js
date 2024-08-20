fdf6d90f1eda2501937b1dff676b831cb37372ce
tann117 <tan.nguyen-van@banvien.com>
2024-08-20 22:11:56 +0700

    fix comment proxy pattern

--- tutorials/proxy-pattern/proxy.pattern.js
+++ tutorials/proxy-pattern/proxy.pattern.js
@@ -1,3 +1,10 @@
+// Proxy pattern dùng để tạo ra một đối tượng thay thế cho một đối tượng khác.
+// Proxy pattern cho phép bạn tạo ra một đối tượng giả để kiểm soát việc truy cập đến đối tượng thật.
+// Proxy pattern giúp bạn kiểm soát việc truy cập đến đối tượng thật, chẳng hạn như:
+// 1. Truy cập đến đối tượng thật từ xa (remote proxy).
+// 2. Truy cập đến đối tượng thật mà không tải nó vào bộ nhớ (virtual proxy).
+// 3. Truy cập đến đối tượng thật mà không thay đổi nó (protection proxy).
+
 class Leader{
     receiveRequest(offer) {
         console.log(`Leadr said !OK::: ${offer}`)
@@ -26,6 +33,7 @@ class Develop {
 //how to use
 const devs = new Develop('Tan1107 upto 2k USD');
 devs.applyFor(new Secretary());
+
 
 
 
